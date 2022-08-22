102298e2bd7e978ad9e091164f5403cf6790a975
Hawkusha <you@example.com>
2022-08-22 05:37:37 +0300

    skills

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const VkBot = require('node-vk-bot-api');
 const { TOKEN, GROUP_ID } = require('./secret');
+const { botName } = require('./config');
 
 const bot = new VkBot({
     token: TOKEN,
     group_id: GROUP_ID,
   })
 
-bot.command('/start', (ctx) => {
-  ctx.reply('Hello!')
-})
+  bot.on((ctx) => {
+    //console.log(ctx);
+    const { message } = ctx;
+    const regBotName = new RegExp(botName, 'i');
+    if (!regBotName.test(message.text)){
+        return;
+    }
+    
+  })
 
 bot.startPolling()

e7007ee883793abbc47e1df0ea47d22d0967673a
Hawkusha <you@example.com>
2022-08-23 01:43:31 +0300

    new callable skill and name

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const VkBot = require('node-vk-bot-api');
 const { TOKEN, GROUP_ID } = require('./secret');
 const { botName } = require('./config');
 
+const skillList = require('./skills/skillList');
+
 const bot = new VkBot({
     token: TOKEN,
     group_id: GROUP_ID,
@@ -12,8 +14,20 @@ const bot = new VkBot({
     const { message } = ctx;
     const regBotName = new RegExp(botName, 'i');
     if (!regBotName.test(message.text)){
-        return;
+        return;  
     }
+
+      for (const callName in skillList) {
+        if (skillList.hasOwnProperty.call(callName)) {
+            const regCallName = new RegExp(callName, 'i');
+            if (regCallName.test(message.text)){
+                console.log(`match: ${callName}`);
+                skillList[callName](message, bot, GROUP_ID);
+                return;
+            }
+        }
+      }
+
     
   })
 
--- skills/mentionAll/index.js
+++ skills/mentionAll/index.js
@@ -1,1 +1,7 @@
-''
+const mentionAll = {
+    callName: '@all',
+    action: (message, bot, group_id) => {
+        console.log ('вызвали');
+    }
+}
+    module.exports = mentionAll;
